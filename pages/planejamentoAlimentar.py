ad85992692690034228950df49fd3344c036f2fc
Pedro Falcão <77029915+falcaovba@users.noreply.github.com>
2024-09-25 23:39:51 -0300

    Correções

--- pages/planejamentoAlimentar.py
+++ pages/planejamentoAlimentar.py
@@ -1,13 +1,15 @@
 import streamlit as st
 import pandas as pd
 
+
+
 pg1, pg2, pg3 = st.columns([0.09,0.09,0.09], gap="small", vertical_alignment="top")
 with pg1:
     st.page_link("streamlit_app.py", label="Home", icon="🏠")
 with pg2:
     st.page_link("pages/planejamentoAlimentar.py", label="Planelamento Alimentar", icon="🥗")
 with pg3:   
-    st.page_link("pages/personalAvaliacao.py", label="Avaliação Personal", icon="💪")
+    st.page_link("pages/planejamentoAlimentar.py", label="Avaliação Personal", icon="💪")
 
     
 st.header("🥗 Planejamento Alimentar", divider="rainbow")
