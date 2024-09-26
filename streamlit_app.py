13109735aed883364299b30d8cdb0e34339fe305
Pedro Falcão <77029915+falcaovba@users.noreply.github.com>
2024-09-25 23:40:18 -0300

    Correções (#7)

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ pg1, pg2, pg3 = st.columns([0.09,0.09,0.09], gap="small", vertical_alignment="to
 with pg1:
     st.page_link("streamlit_app.py", label="Home", icon="🏠")
 with pg2:
-    st.page_link("pages/planejamentoAlimentar.py", label="Planelamento Alimentar", icon="🥗")
+    st.page_link("pages/planejamentoAlimentar.py", label="Planelamento Alimentar", icon="🥗",)
 with pg3:   
-    st.page_link("pages/Avaliação Personal.py", label="Avaliação Personal", icon="💪")
+    st.page_link("pages/personalAvaliacao.py", label="Avaliação Personal", icon="💪")
 
 st.header("🏛️ Home", divider="rainbow")
