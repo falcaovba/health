daeac3192927a0c259c7c694e04b083c280d9698
Pedro Falcão <77029915+falcaovba@users.noreply.github.com>
2024-09-25 23:35:12 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(page_title='Health - Pedro Henrique', page_icon='🏋️‍♀️',layout="wide",initial_sidebar_state="collapsed")
+
 pg1, pg2, pg3 = st.columns([0.09,0.09,0.09], gap="small", vertical_alignment="top")
 with pg1:
     st.page_link("streamlit_app.py", label="Home", icon="🏠")
